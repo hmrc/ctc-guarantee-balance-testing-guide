0e1db69982c3c79eb1eece96077f2cc8468602f7
Tim Squires <tim@crystalmark.co.uk>
2022-02-07 11:59:16 +0000

    CGBA-104 changed ruby version to 2.6 and node version to match build-jobs

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -22,7 +22,7 @@ import sys.process._
 class BuildSpec extends WordSpec with Matchers {
   "Building the content" should {
     "produce static files" in {
-      val result = "bundle install" #&& Process("bundle exec middleman build --build-dir=public/ --clean", None) !
+      val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None) !
 
       result shouldBe 0
     }
