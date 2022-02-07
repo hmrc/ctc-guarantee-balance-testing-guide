e6da94814c3912fbcf9ba753beb9415aa0768472
Tim Squires <tim@crystalmark.co.uk>
2022-02-07 14:25:58 +0000

    CGBA-104 added base path to build spec

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import sys.process._
 class BuildSpec extends WordSpec with Matchers {
   "Building the content" should {
     "produce static files" in {
-      val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None) !
+      val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None, "BASE_PATH" -> "/guides/ctc-guarantee-balance-testing-guide/") !
 
       result shouldBe 0
     }
